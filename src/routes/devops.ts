4c13bf1aa799c6bf6683a618085a8b9aaa3f3636
Shkar T. Noori <shkarface@gmail.com>
2021-10-14 13:12:11 +0000

    Configure sentry logging

--- src/routes/devops.ts
+++ src/routes/devops.ts
@@ -8,19 +8,23 @@ import * as Sentry from '@sentry/node'
 export default (opts: { server: any, bootStartTime: any, config: Configurable }) => {
   const { server, bootStartTime, config } = opts
 
-  // Only initialize sentry if we have it configured.
-  if (config.sentry) { Sentry.init(config.sentry) }
+  // Only initialize sentry if we have it configured and not in development.
+  if (process.env.NODE_ENV !== 'development') {
+    if (config.sentry) {
+      Sentry.init(config.sentry)
 
-  server.addHook(
-    'onError',
-    (request: any, reply: any, error: unknown, done: any) => {
-      // Only send Sentry errors when not in development
-      if (process.env.NODE_ENV !== 'development') {
-        Sentry.captureException(error)
-      }
-      done()
-    }
-  )
+      server.addHook(
+        'onError',
+        (request: any, reply: any, error: unknown, done: any) => {
+          // Only send Sentry errors when not in development
+
+          Sentry.captureException(error)
+          done()
+        }
+      )
+      console.log('Configuring sentry')
+    } else console.log('[WARN] Sentry not configured')
+  } else console.log('[INFO] Skipping sentry in development')
 
   server.get('/status', (request: FastifyRequest, reply: FastifyReply) => {
     const rootDir: string = pkgDir.sync() as string
